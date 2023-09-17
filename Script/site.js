65d6efa022a098a0196cef9d531dd6d506404552
rabia amin <rabia10marchgmail.com>
2023-09-17 15:57:48 +0500

    git repo

---
+++ Script/site.js
@@ -0,0 +1,21 @@
+const menuIcon = document.querySelector('.menu-icon');
+const categoryListWrap = document.querySelector('.category-list-wrap');
+const categoryMenuContainer = document.querySelector('.category-menu-container');
+const dropdownSearchBtn = document.querySelector('.dropdown-search-btn');
+menuIcon.addEventListener('click',()=>{
+    if(categoryListWrap.style.display === 'none'){
+        categoryListWrap.style.display = 'flex';
+    }
+    else{
+        categoryListWrap.style.display = 'none';
+    }
+});
+
+dropdownSearchBtn.addEventListener('click',()=>{
+    if(categoryMenuContainer.style.display === 'none'){
+        categoryMenuContainer.style.display = 'flex';
+    }
+    else{
+        categoryMenuContainer.style.display = 'none';
+    }
+})
