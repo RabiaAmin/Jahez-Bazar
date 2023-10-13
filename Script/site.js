696ed6553a992e015accbcb0a7d995d0472da199
rabia amin <rabia10marchgmail.com>
2023-10-13 15:10:08 +0500

    Added Panels

--- Script/site.js
+++ Script/site.js
@@ -5,9 +5,18 @@ const dropdownSearchBtn = document.querySelector('.dropdown-search-btn');
 menuIcon.addEventListener('click',()=>{
     if(categoryListWrap.style.display === 'none'){
         categoryListWrap.style.display = 'flex';
+     
     }
     else{
         categoryListWrap.style.display = 'none';
+    }
+});
+
+
+// Close the menu when you click anywhere else on the document
+document.addEventListener("click", function(e) {
+    if (e.target !== menuIcon && e.target !== categoryListWrap) {
+        categoryListWrap.style.display = "none";
     }
 });
 
